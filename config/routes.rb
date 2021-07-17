a9b039848949164a5834a02b2b9b6b54c9aae0c1
actualquak <>
2021-07-17 10:36:37 +1000

    bootstrap login/signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/signout", to: 'devise/sessions#destroy', as: :signout
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
