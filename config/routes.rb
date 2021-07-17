c54c9afbeb93133ad7cec538fe6b1475e7741650
actualquak <>
2021-07-17 10:46:35 +1000

    add name column to users table, create RegistraionsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/signout", to: 'devise/sessions#destroy', as: :signout
     get "/signin", to: 'devise/sessions#new'
-    get "/signup", to: 'devise/registrations#new'
   end
-  devise_for :users
+  devise_for :users, :controllers => {registrations => "registraions"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   get "/index", to: "pages#index"
