007db7453036b5a99191f93a5503ec7cca04f3ca
actualquak <>
2021-07-18 14:33:04 +1000

    styling stuff, bootstrap is good lul

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/signout", to: 'devise/sessions#destroy', as: :signout
     get "/signin", to: 'devise/sessions#new'
-    get "/signup", to: 'devise/registrations#new'
+    get "/new-profile", to: 'devise/registrations#new'
+    get "/edit-profile", to: 'devise/registrations#edit'
+    get "/delete-profile", to: 'devise/registrations#destroy'
   end
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :articles do
