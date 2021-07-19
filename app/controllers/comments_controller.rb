5a0b78f75d76706bd489418c043298d3354d418d
actualquak <>
2021-07-19 19:07:05 +1000

    comments on articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,13 @@ class CommentsController < ApplicationController
   def create
     if params[:comment][:parent_id].to_i > 0
       parent = Comment.find_by_id(params[:comment].delete(:parent_id))
+      Comment.find_by_id(params[:comment].delete(:article_id))
       @comment = parent.children.build(comment_params)
       @comment.article_id = parent.article_id
     else
       @comment = Comment.new(comment_params)
+      @comment.article_id = params[:comment][:article_id]
     end
-
     if @comment.save
       flash[:success] = 'Your comment was successfully added!'
       redirect_to root_path
@@ -40,7 +41,6 @@ class CommentsController < ApplicationController
     end
   end
   private
-
     def comment_params
       params.require(:comment).permit(:content)
     end
